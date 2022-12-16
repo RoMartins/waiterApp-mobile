52afb4549d05b69fb157c56cb3cc00ce4ffb05da
RoMartins <rodrigomartins_santos1804@hotmail.com>
2022-12-16 13:40:20 -0300

    first commit

---
+++ src/Main/index.tsx
@@ -0,0 +1,16 @@
+import { Text } from '../components/Text';
+import { Container } from './styles';
+
+
+export function Main() {
+  return (
+    <Container>
+
+      <Text>
+      Main Renderizou
+      </Text>
+    </Container>
+  );
+}
+
+
